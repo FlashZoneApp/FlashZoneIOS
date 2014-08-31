726f11c6c456f85a10c9cf420e7b4b6bf1af4fb7
Denny Kwon <info@tsunami234.com>
2014-08-30 20:43:09 -0400

    uiview category for taking screenshots

---
+++ FlashZone/Register/FZButtonTag.h
@@ -0,0 +1,13 @@
+//
+//  FZButtonTag.h
+//  FlashZone
+//
+//  Created by Dan Kwon on 8/30/14.
+//  Copyright (c) 2014 FlashZone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FZButtonTag : UIButton
+
+@end
