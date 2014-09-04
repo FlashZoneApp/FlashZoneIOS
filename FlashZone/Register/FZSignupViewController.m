b6711ef525908fa724d00d0d1492805a50b8e74e
Denny Kwon <info@tsunami234.com>
2014-09-04 19:05:23 -0400

    added generic social network login buttons

--- FlashZone/Register/FZSignupViewController.m
+++ FlashZone/Register/FZSignupViewController.m
@@ -55,16 +55,21 @@
     static CGFloat padding = 8.0f;
     CGFloat w = frame.size.width-2*padding;
     
-    NSArray *socialogins = @[@"loginFacebook.png", @"loginTwitter.png", @"loginGoogle.png", @"loginLinkedin.png", @"loginReddit.png"];
+    NSArray *socialogins = @[@"Facebook", @"Twitter", @"Google", @"Linkedin", @"Reddit"];
     for (int i=0; i<socialogins.count; i++){
-        UIImage *loginImage = [UIImage imageNamed:socialogins[i]];
+        NSString *network = socialogins[i];
+        UIImage *loginImage = [UIImage imageNamed:[NSString stringWithFormat:@"btn%@.png", network]];
         UIButton *btnLogin = [UIButton buttonWithType:UIButtonTypeCustom];
         btnLogin.tag = 1000+i;
+        btnLogin.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
         btnLogin.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
         btnLogin.frame = CGRectMake(0.0f, y, loginImage.size.width, loginImage.size.height);
         btnLogin.center = CGPointMake(0.5f*frame.size.width+1, btnLogin.center.y);
         [btnLogin setBackgroundImage:loginImage forState:UIControlStateNormal];
         [btnLogin addTarget:self action:@selector(signupSocialNetwork:) forControlEvents:UIControlEventTouchUpInside];
+        [btnLogin setTitle:[NSString stringWithFormat:@"            Connect with %@", network] forState:UIControlStateNormal];
+        [btnLogin setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        btnLogin.titleLabel.font = [UIFont systemFontOfSize:16.0f];
         [view addSubview:btnLogin];
         y += loginImage.size.height+padding;
     }
