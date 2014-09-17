3979fb701c84ca7ce9b4ca1177c886fc97ff7131
Denny Kwon <info@tsunami234.com>
2014-09-17 15:05:30 -0400

    reset social network colors to hex strings from Dylan

--- FlashZone/Config.h
+++ FlashZone/Config.h
@@ -13,26 +13,26 @@
 #define kBaseUrl @"http://flash-zone.appspot.com/"
 
 // Social Integration
-#define kTwitterAPI @"https://api.twitter.com/1.1/"
-#define kFacebookAPI @"https://graph.facebook.com/v2.1/"
-#define kFacebookAppID @"552474471525097"
-#define kFacebookPermissions @[@"email", @"user_likes", @"user_interests"]
-#define kLinkedInAPI @"https://api.linkedin.com/v1/"
-#define kLinkedInClientID @"77clc8s96rvbxr"
-#define kLinkedInClientSecret @"bNldG6BFfJZTMPtu"
-#define kLinkedInState @"DCEEFWF45453sdffef424"
-#define kGooglePlusClientID @"434470043767-1dbtcobtt0i4kokfor9lujjthgpkp3ih.apps.googleusercontent.com"
+#define kTwitterAPI             @"https://api.twitter.com/1.1/"
+#define kFacebookAPI            @"https://graph.facebook.com/v2.1/"
+#define kFacebookAppID          @"552474471525097"
+#define kFacebookPermissions    @[@"email", @"user_likes", @"user_interests"]
+#define kLinkedInAPI            @"https://api.linkedin.com/v1/"
+#define kLinkedInClientID       @"77clc8s96rvbxr"
+#define kLinkedInClientSecret   @"bNldG6BFfJZTMPtu"
+#define kLinkedInState          @"DCEEFWF45453sdffef424"
+#define kGooglePlusClientID     @"434470043767-1dbtcobtt0i4kokfor9lujjthgpkp3ih.apps.googleusercontent.com"
 
 
 
 
 //Colors:
 #define kOrange [UIColor colorWithRed:255.0f/255.0f green:173.0f/255.0f blue:15.0f/255.0f alpha:1.0f]
-#define kFacebookBlue [UIColor colorWithRed:25.0f/255.0f green:117.0f/255.0f blue:185.0f/255.0f alpha:1.0f]
-#define kTwitterBlue [UIColor colorWithRed:51.0f/255.0f green:188.0f/255.0f blue:185.0f/235.0f alpha:1.0f]
-#define kLinkedinBlue [UIColor colorWithRed:0.0f/255.0f green:123.0f/255.0f blue:181.0f/235.0f alpha:1.0f]
-#define kGoogleRed [UIColor colorWithRed:221.0f/255.0f green:75.0f/255.0f blue:57.0f/235.0f alpha:1.0f]
-#define kRedditRed [UIColor colorWithRed:255.0f/255.0f green:69.0f/255.0f blue:0.0f/235.0f alpha:1.0f]
+#define kFacebookBlue   @"#3b5998"
+#define kTwitterBlue    @"#55acee"
+#define kLinkedinBlue   @"#007bb5"
+#define kGoogleRed      @"#dd4b39"
+#define kRedditRed      @"#ff4500"
 
 
 #define kShowProfileDetailsNotification @"ShowProfileDetails"
