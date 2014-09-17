a928cab1a86b838dcf56601a3540d34ef1351e2a
Denny Kwon <info@tsunami234.com>
2014-09-17 13:29:51 -0400

    added color constants for social network buttons

--- FlashZone/Config.h
+++ FlashZone/Config.h
@@ -28,6 +28,11 @@
 
 //Colors:
 #define kOrange [UIColor colorWithRed:255.0f/255.0f green:173.0f/255.0f blue:15.0f/255.0f alpha:1.0f]
+#define kFacebookBlue [UIColor colorWithRed:25.0f/255.0f green:117.0f/255.0f blue:185.0f/255.0f alpha:1.0f]
+#define kTwitterBlue [UIColor colorWithRed:51.0f/255.0f green:188.0f/255.0f blue:185.0f/235.0f alpha:1.0f]
+#define kLinkedinBlue [UIColor colorWithRed:0.0f/255.0f green:123.0f/255.0f blue:181.0f/235.0f alpha:1.0f]
+#define kGoogleRed [UIColor colorWithRed:221.0f/255.0f green:75.0f/255.0f blue:57.0f/235.0f alpha:1.0f]
+#define kRedditRed [UIColor colorWithRed:255.0f/255.0f green:69.0f/255.0f blue:0.0f/235.0f alpha:1.0f]
 
 
 #define kShowProfileDetailsNotification @"ShowProfileDetails"
