3a8a971ab741bd42c0aac631e9166717904cc3a9
Denny Kwon <info@tsunami234.com>
2014-09-25 12:55:15 -0400

    uiview subclass for hilighting cell when selecting from search table

---
+++ FlashZone/Register/FZSelectedTagView.h
@@ -0,0 +1,14 @@
+//
+//  FZSelectedTagView.h
+//  FlashZone
+//
+//  Created by Dan Kwon on 9/25/14.
+//  Copyright (c) 2014 FlashZone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FZSelectedTagView : UIView
+
+@property (strong, nonatomic) UILabel *lblTitle;
+@end
---
+++ FlashZone/Register/FZSelectedTagView.m
@@ -0,0 +1,39 @@
+//
+//  FZSelectedTagView.m
+//  FlashZone
+//
+//  Created by Dan Kwon on 9/25/14.
+//  Copyright (c) 2014 FlashZone. All rights reserved.
+//
+
+#import "FZSelectedTagView.h"
+
+@implementation FZSelectedTagView
+@synthesize lblTitle;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.lblTitle = [[UILabel alloc] initWithFrame:CGRectMake(14, 10, frame.size.width-20.0f, 22.0f)];
+        self.lblTitle.textColor = [UIColor whiteColor];
+        self.lblTitle.font = [UIFont boldSystemFontOfSize:18.0f];
+        [self addSubview:self.lblTitle];
+        
+        UIImageView *checkMark = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"iconCheckMark.png"]];
+        checkMark.center = CGPointMake(frame.size.width-30.0f, self.lblTitle.center.y);
+        [self addSubview:checkMark];
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
